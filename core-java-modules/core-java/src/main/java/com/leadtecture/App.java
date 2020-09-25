b16047da56b19cfd3cccd0d1a103a4faad60d9bd
shaynegao <gaoweiwei01@gmail.com>
2020-09-25 10:17:27 +0800

    添加一个纯java的项目core-java

---
+++ core-java-modules/core-java/src/main/java/com/leadtecture/App.java
@@ -0,0 +1,13 @@
+package com.leadtecture;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
